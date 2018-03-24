27d21f892d0c0ee3e438894d3892fc09db5bae3e
Troy Miles <rockncoder@gmail.com>
2018-03-24 10:34:16 -0700

    adds error handler - with errors.

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,15 +1,21 @@
-import {CREATE_CONTACT_REQUEST, CREATE_CONTACT_SUCCESS} from '../types';
+import {
+  CREATE_CONTACT_REQUEST,
+  CREATE_CONTACT_SUCCESS, CREATE_CONTACT_FAILURE
+} from '../types';
 
-export const createContactRequest = contact => {
-  debugger;
-  return ({
+export const createContactRequest = contact => ({
     type: CREATE_CONTACT_REQUEST,
     contact
   })
-};
+;
 
 export const createContactSuccess = contact => ({
   type: CREATE_CONTACT_SUCCESS,
   contact
+});
+
+export const createContactFailure = error => ({
+  type: CREATE_CONTACT_FAILURE,
+  error
 });
 
--- src/sagas/contactSaga.js
+++ src/sagas/contactSaga.js
@@ -1,11 +1,16 @@
 import axios from 'axios';
 import {call, put} from 'redux-saga/effects';
-import {createContactSuccess} from '../actions/user';
+import {createContactFailure, createContactSuccess} from '../actions/user';
 
 const url = 'https://tekadept-vehicles.herokuapp.com/contact';
 
 export function* createContactSaga(action) {
-  const contact = yield call(axios.post, url, action.contact);
-  console.info(JSON.stringify(contact));
-  yield put(createContactSuccess(contact));
+  try {
+    const contact = yield call(axios.post, url, action.contact);
+    console.info(JSON.stringify(contact));
+    yield put(createContactSuccess(contact));
+  } catch (err) {
+    console.info(err);
+    yield put(createContactFailure, err);
+  }
 }
